80dd526a6dce78b2c27a9b776535ae30985b3492
vrajpatel23 <113646894+vrajpatel23@users.noreply.github.com>
2022-09-15 22:36:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB422 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: vraj bhavesbhai patel______________________ Student ID: 147266209______________ Date: ________________
+*  Cyclic Link: _______________________________________________________________
+*
+********************************************************************************/ 
+
+
 const exp = require("express");
 const app = exp();
 const bodyparser = require("body-parser");
